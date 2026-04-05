8d39f426be5fda3367c9e0e0f3458d21245f514b
vaman <georgevaman@MacBook-Pro-5.local>
2026-04-05 23:20:23 +0300

    first

---
+++ Linear_Regression_Model/main.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+
+# Data prep
+data_root = "https://github.com/ageron/data/raw/main/"
+lifesat = pd.read_csv(data_root + "lifesat/lifesat.csv")
+x=lifesat[["GDP per capita (USD)"]].values
+y=lifesat[["Life satisfaction"]].values
+
+
+#Data visualisation
+lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+plt.show()
+
+
+# Select a linear model
+model = LinearRegression()
+
+# Train the model
+model.fit(x, y)
+
+# Make a new prediction for cyprus
+x_new = [[37_655.2]] # Cyprus GDP per capita in 2020
+print(model.predict(x_new))
