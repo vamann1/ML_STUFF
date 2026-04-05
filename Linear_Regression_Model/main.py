423009666e74af220921d9d0f1b38927bf96fdab
vaman <georgevaman@MacBook-Pro-5.local>
2026-04-05 23:32:05 +0300

    show regression line

--- Linear_Regression_Model/main.py
+++ Linear_Regression_Model/main.py
@@ -23,6 +23,16 @@ model = LinearRegression()
 # Train the model
 model.fit(x, y)
 
+ax = lifesat.plot(kind='scatter', grid=True, x="GDP per capita (USD)", y="Life satisfaction")
+plt.axis([23_500, 62_500, 4, 9])
+
+x_line = np.linspace(23_500, 62_500, 200)
+y_line = model.predict(x_line.reshape(-1, 1))
+ax.plot(x_line, y_line, color='red', linewidth=2, label='Linear fit')
+ax.legend()
+
+plt.show()
+
 # Make a new prediction for cyprus
 x_new = [[37_655.2]] # Cyprus GDP per capita in 2020
 print(model.predict(x_new))
